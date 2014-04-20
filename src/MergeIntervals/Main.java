c08d10873e5ab555586304f81d4f9ab6f989a798
Yiming Yue <yueyiming2009@gmail.com>
2014-04-20 21:50:28 +0800

    check in today's code

---
+++ src/MergeIntervals/Main.java
@@ -0,0 +1,24 @@
+package MergeIntervals;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ymyue on 4/19/14.
+ */
+public class Main {
+    public static void main(String [] args) {
+        Solution s = new Solution();
+        ArrayList<Interval> list = new ArrayList<>();
+//        list.add(new Interval(2, 3));
+//        list.add(new Interval(5, 5));
+//        list.add(new Interval(2,2));
+        list.add(new Interval(1, 4));
+        list.add(new Interval(2, 5));
+        ArrayList<Interval> rList = s.merge(list);
+        for (Interval inter : rList) {
+            System.out.print(inter.start);
+            System.out.print("\t");
+            System.out.println(inter.end);
+        }
+    }
+}
---
+++ src/NQueen/Solution.java
@@ -0,0 +1,60 @@
+package NQueen;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ymyue on 4/20/14.
+ */
+public class Solution {
+    public ArrayList<String[]> solveNQueens(int n) {
+        ArrayList<String[]> rList = new ArrayList<String[]> ();
+        String [] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++)
+                sb.append(".");
+            str[i] = sb.toString();
+        }
+        search(str, n, rList, n);
+        return rList;
+    }
+    private void search (String [] str, int n, ArrayList<String[]> rList, int m) {
+        boolean [][] matrix = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (str[i].charAt(j) == 'Q') {
+                    for (int k = 0; k < n; k++) {
+                        matrix[k][j] = true;
+                        matrix[i][k] = true;
+                        if (i+k < n && j+k < n)
+                            matrix[i+k][j+k] = true;
+                        if (i-k >= 0 && j-k >= 0)
+                            matrix[i-k][j-k] = true;
+                        if (i+k < n && j-k >= 0)
+                            matrix[i+k][j-k] = true;
+                        if (i-k >= 0 && j+k < n)
+                            matrix[i-k][j+k] = true;
+                    }
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            StringBuilder sb = new StringBuilder(str[n-m]);
+            if (!matrix[n-m][j]) {
+                sb.setCharAt(j, 'Q');
+                str[n-m] = sb.toString();
+                if (m == 1) {
+                    String [] r = new String[n];
+                    for (int k = 0; k < n; k++) {
+                        r[k] = new String(str[k]);
+                    }
+                    rList.add(r);
+                } else {
+                    search(str, n, rList, m-1);
+                }
+                sb.setCharAt(j, '.');
+                str[n-m] = sb.toString();
+            }
+        }
+    }
+}
