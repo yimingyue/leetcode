1fc06aa0d2e913a4a35e20a17e267523bacbdedc
Yiming Yue <ymyue@yahoo-inc.com>
2015-11-14 23:48:06 -0800

    add 3Sum

---
+++ src/main/java/leetcode/3Sum/Solution.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> rList = new ArrayList<> ();
+        if (nums == null || nums.length == 0)
+            return rList;
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length) {
+            int j = i+1;
+            int k = nums.length-1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<> ();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    rList.add(list);
+                    j++;
+                    while (nums[j] == nums[j-1] && j < k)
+                        j++;
+                    k--;
+                    while (nums[k] == nums[k+1] && j < k)
+                        k--;
+                } else if (sum < 0) {
+                    j++;
+                } else
+                    k--;
+            }
+            i++;
+            while (i < nums.length && nums[i] == nums[i-1])
+                i++;
+        }
+        return rList;
+    }
+}
